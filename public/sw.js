fd61e9e585a07ea1775da80855e2832036fb9bc8
mahendrayadav2327 <mahendrayadav800367@gmail.com>
2026-09-21 12:06:08 +0400

    Add files via upload

---
+++ public/sw.js
@@ -0,0 +1,8 @@
+const CACHE='rcc-stocks-shell-v1';
+const SHELL=['/','/manifest.json','/icons/icon-192.png','/icons/icon-512.png'];
+self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)));self.skipWaiting()});
+self.addEventListener('activate',e=>{e.waitUntil(self.clients.claim())});
+self.addEventListener('fetch',e=>{
+  if(e.request.method!=='GET')return;
+  e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request)));
+});
